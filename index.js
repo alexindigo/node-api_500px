4a4752c5f7700b0c3fb3fcc8bf1689beec4becc4
Alex Indigo <iam@alexindigo.com>
2012-08-26 13:05:59 -0700

    Added getAccessToken and tool methods (_get, _post) + unit tests. Improvements.

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ var util         = require('util')
       secret     : '',
       callback   : ''
     }
+  , bareFunction = function(){}
   ;
 
 module.exports = api_500px;
@@ -48,19 +49,86 @@ function api_500px(options)
     options.signature || config.signature
   );
 
+  // prepare place for user's info
+  this.data = {};
+
   // change header separator to make it work with 500px
   this.OA._oauthParameterSeperator = options.separator || config.separator;
 
   return this;
 };
 
-// Sends auth request to the api service,
-// upon receiving verification token
-// creates verification handler
+// Sends auth request
 api_500px.prototype.authRequest = function api500pxAuthRequest(callback)
 {
+  // keep callbacks optional
+  callback = callback || bareFunction;
+
   // request token
-  return this.OA.getOAuthRequestToken(callback);
+  this.OA.getOAuthRequestToken(function handler_api500pxAuthRequest(err, auth_token, auth_secret, results)
+  {
+    if (err) return callback(err);
+
+    // TODO: Check if there is any overlapping
+    this.data.auth_token  = auth_token;
+    this.data.auth_secret = auth_secret;
+
+    callback(null, auth_token, auth_secret, results);
+  }.bind(this));
+}
+
+// Sends access token request
+api_500px.prototype.getAccessToken = function api500pxGetAccessToken(verifier, callback)
+{
+  // keep callbacks optional
+  callback = callback || bareFunction;
+
+  // get access token
+  this.OA.getOAuthAccessToken(this.data.auth_token, this.data.auth_secret, verifier, function handler_api500pxGetAccessToken(err, access_token, access_secret, results)
+  {
+    if (err) return callback(err);
+
+    // cleanup auth token
+    delete this.data.auth_token;
+    delete this.data.auth_secret;
+
+    // store access token and secret
+    this.data.access_token  = access_token;
+    this.data.access_secret = access_secret;
+    // it's not normalized data, therefore underscore
+    this.data._details      = results;
+
+    callback(null, access_token, access_secret, results);
+  }.bind(this));
 }
 
 
+/*
+ * Tool functions
+ */
+
+// TODO: Streaming JSON parser?
+api_500px.prototype._get = function api500pxGet(url, callback)
+{
+  return this.OA.get(url, this.data.access_token, this.data.access_secret, callback);
+}
+
+api_500px.prototype._post = function api500pxPost(url, body, contentType, callback)
+{
+  // check for callback
+  if (typeof body == 'function')
+  {
+    callback = body;
+    body     = null;
+  }
+  else if (typeof contentType == 'function')
+  {
+    callback    = contentType;
+    contentType = null;
+  }
+
+  // content type
+  contentType = contentType || 'application/json';
+
+  return this.OA.post(url, this.data.access_token, this.data.access_secret, body, contentType, callback);
+}
--- test/unit/test-base.js
+++ test/unit/test-base.js
@@ -1,10 +1,10 @@
-var assert        = require('assert')
-  , test          = require('utest')
+var assert       = require('assert')
+  , test         = require('utest')
 
   // package thingy
-  , Api500px      = require('../../index')
+  , Api500px     = require('../../index')
   , api // instance
-  , emptyFunction = function(){}
+  , bareFunction = function(){}
   ;
 
 test('api_500px',
@@ -13,21 +13,20 @@ test('api_500px',
   {
     api = new Api500px(
     {
-      key: 'api_500px_test_key_token',
-      secret: 'api_500px_test_secret_token',
-      callback: 'http://api_500px_test_callback_url',
+      key: 'api_test_key_token',
+      secret: 'api_test_secret_token',
+      callback: 'http://api_test_callback_url',
     });
   },
 
   'check api instance': function()
   {
-
     assert.equal(api.OA._requestUrl, 'https://api.500px.com/v1/oauth/request_token');
     assert.equal(api.OA._accessUrl, 'https://api.500px.com/v1/oauth/access_token');
-    assert.equal(api.OA._consumerKey, 'api_500px_test_key_token');
-    assert.equal(api.OA._consumerSecret, 'api_500px_test_secret_token');
+    assert.equal(api.OA._consumerKey, 'api_test_key_token');
+    assert.equal(api.OA._consumerSecret, 'api_test_secret_token');
     assert.equal(api.OA._version, '1.0A');
-    assert.equal(api.OA._authorize_callback, 'http://api_500px_test_callback_url');
+    assert.equal(api.OA._authorize_callback, 'http://api_test_callback_url');
     assert.equal(api.OA._signatureMethod, 'HMAC-SHA1');
     assert.equal(api.OA._oauthParameterSeperator, ', ');
   },
@@ -38,9 +37,155 @@ test('api_500px',
     api.OA.getOAuthRequestToken = function(callback)
     {
       assert.ok(typeof callback == 'function');
+
+      // emulate return data
+      callback(null, 'api_test_auth_key_token', 'api_test_auth_secret_token', {status: 'ok'});
     };
 
-    api.authRequest(emptyFunction);
-  }
+    api.authRequest(function(err, auth_token, auth_secret, results)
+    {
+      // check return values
+      assert.ifError(err);
+      assert.equal(auth_token, 'api_test_auth_key_token');
+      assert.equal(auth_secret, 'api_test_auth_secret_token');
+      assert.equal(results.status, 'ok');
 
+      // check stored values
+      assert.equal(api.data.auth_token, 'api_test_auth_key_token');
+      assert.equal(api.data.auth_secret, 'api_test_auth_secret_token');
+    });
+  },
+
+  'get access token': function()
+  {
+    // set auth_token
+    api.data.auth_token  = 'api_test_auth_key_token';
+    api.data.auth_secret = 'api_test_auth_secret_token';
+
+    // override OAuth
+    api.OA.getOAuthAccessToken = function(token, secret, verifier, callback)
+    {
+      assert.equal(token, 'api_test_auth_key_token');
+      assert.equal(secret, 'api_test_auth_secret_token');
+      assert.equal(verifier, 'api_test_auth_verifier_token');
+      assert.ok(typeof callback == 'function');
+
+      // emulate return data
+      callback(null, 'api_test_access_key_token', 'api_test_access_secret_token', {user: 'user'});
+    };
+
+    api.getAccessToken('api_test_auth_verifier_token', function(err, access_token, access_secret, results)
+    {
+      // check return values
+      assert.ifError(err);
+      assert.equal(access_token, 'api_test_access_key_token');
+      assert.equal(access_secret, 'api_test_access_secret_token');
+      assert.equal(results.user, 'user');
+
+      // check stored values
+      assert.equal(api.data.access_token, 'api_test_access_key_token');
+      assert.equal(api.data.access_secret, 'api_test_access_secret_token');
+      assert.equal(api.data._details.user, 'user');
+    });
+  },
+
+  // Tool functions
+
+  'check _get': function()
+  {
+    // set auth_token
+    api.data.access_token  = 'api_test_access_key_token';
+    api.data.access_secret = 'api_test_access_key_token';
+
+    // override OAuth
+    api.OA.get = function(url, token, secret, callback)
+    {
+      assert.equal(url, 'api_test_get_url');
+      assert.equal(token, 'api_test_access_key_token');
+      assert.equal(secret, 'api_test_access_key_token');
+      assert.ok(typeof callback == 'function');
+
+      return 'api_test_get_return';
+    };
+
+    var r = api._get('api_test_get_url', bareFunction);
+
+    // check return values
+    assert.equal(r, 'api_test_get_return');
+  },
+
+  'check _post, short version': function()
+  {
+    // set auth_token
+    api.data.access_token  = 'api_test_access_key_token';
+    api.data.access_secret = 'api_test_access_key_token';
+
+    // override OAuth
+    api.OA.post = function(url, token, secret, body, contentType, callback)
+    {
+      assert.equal(url, 'api_test_post_url');
+      assert.equal(token, 'api_test_access_key_token');
+      assert.equal(secret, 'api_test_access_key_token');
+      assert.equal(body, null);
+      assert.equal(contentType, 'application/json');
+      assert.ok(typeof callback == 'function');
+
+      return 'api_test_post_return';
+    };
+
+    var r = api._post('api_test_post_url', bareFunction);
+
+    // check return values
+    assert.equal(r, 'api_test_post_return');
+  },
+
+  'check _post, short +body version': function()
+  {
+    // set auth_token
+    api.data.access_token  = 'api_test_access_key_token';
+    api.data.access_secret = 'api_test_access_key_token';
+
+    // override OAuth
+    api.OA.post = function(url, token, secret, body, contentType, callback)
+    {
+      assert.equal(url, 'api_test_post_url');
+      assert.equal(token, 'api_test_access_key_token');
+      assert.equal(secret, 'api_test_access_key_token');
+      assert.equal(body.param, 'value');
+      assert.equal(contentType, 'application/json');
+      assert.ok(typeof callback == 'function');
+
+      return 'api_test_post_return';
+    };
+
+    var r = api._post('api_test_post_url', {param: 'value'}, bareFunction);
+
+    // check return values
+    assert.equal(r, 'api_test_post_return');
+  },
+
+  'check _post, full version': function()
+  {
+    // set auth_token
+    api.data.access_token  = 'api_test_access_key_token';
+    api.data.access_secret = 'api_test_access_key_token';
+
+    // override OAuth
+    api.OA.post = function(url, token, secret, body, contentType, callback)
+    {
+      assert.equal(url, 'api_test_post_url');
+      assert.equal(token, 'api_test_access_key_token');
+      assert.equal(secret, 'api_test_access_key_token');
+      assert.equal(body, 'api_test_post_body');
+      assert.equal(contentType, 'text/xml');
+      assert.ok(typeof callback == 'function');
+
+      return 'api_test_post_return';
+    };
+
+    var r = api._post('api_test_post_url', 'api_test_post_body', 'text/xml', bareFunction);
+
+    // check return values
+    assert.equal(r, 'api_test_post_return');
+  }
 });
